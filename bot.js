d68ebdaf9e960bbf5651b92365cfa4d8675a3b99
erwetghngmjwerfgth <41790254+7S0@users.noreply.github.com>
2018-12-24 23:34:06 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -33,6 +33,24 @@ client.user.setGame(`server BRG `,"http://twitch.tv/S-F")
   console.log('')
 });
 
+client.on('message', message => {
+    if (message.content === ('$bot')) {
+    message.channel.send({
+        embed: new Discord.RichEmbed()
+            .setAuthor(client.user.username,client.user.avatarURL)
+            .setThumbnail(client.user.avatarURL)
+            .setColor('RANDOM')
+            .addField('**Bot Ping**🚀 :' , [`${Date.now() - message.createdTimestamp}` + 'MS'], true)
+            .addField('**Servers**📚 :', [client.guilds.size], true)
+            .addField('**Channels**📝 :' , `[ ${client.channels.size} ]` , true)
+            .addField('**Users**🔮 :' ,`[ ${client.users.size} ]` , true)
+            .addField('**Bot Name**🔰 :' , `[ ${client.user.tag} ]` , true)
+            .addField('**Bot Owner**👑 :' , `[<@368124062997938177>]` , true)
+            .setFooter(message.author.username, message.author.avatarURL)
+    })
+}
+});
+
 
 
 
