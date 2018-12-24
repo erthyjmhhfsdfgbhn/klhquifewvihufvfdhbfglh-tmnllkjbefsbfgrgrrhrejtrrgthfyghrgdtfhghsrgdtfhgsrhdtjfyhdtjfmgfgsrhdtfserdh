b02844af34eeceee382c971b392756c14797d4d8
erwetghngmjwerfgth <41790254+7S0@users.noreply.github.com>
2018-12-25 01:38:01 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -224,12 +224,13 @@ client.on("message", message => {
   let args = message.content.split(" ").slice(1);
   var argresult = args.join(' '); 
   message.guild.members.filter(m => m.presence.status !== 'all').forEach(m => {
- m.send(${argresult}\n ${m});
+ m.send(${argresult}\n $){m});
 })
- message.channel.send(\${message.guild.members.filter(m => m.presence.status !== 'all').size}` : عدد الاعضاء المستلمين`); 
+ message.channel.send(${message.guild.members.filter(m => m.presence.status !== 'all').size}: عدد الاعضاء المستلمين); 
  message.delete(); 
 };
 });
+
 
 
 client.login(process.env.BOT_TOKEN);
