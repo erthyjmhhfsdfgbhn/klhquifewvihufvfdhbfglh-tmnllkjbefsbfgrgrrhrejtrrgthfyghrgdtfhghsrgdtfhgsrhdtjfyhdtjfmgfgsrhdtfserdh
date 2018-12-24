a1e8723db0f01041579c4450cd66b26c761a125e
erwetghngmjwerfgth <41790254+7S0@users.noreply.github.com>
2018-12-25 00:58:17 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -217,5 +217,20 @@ client.on('message', msg => {
   }
 });
 
+client.on("message", message => {
+
+            if (message.content.startsWith($ + "bc")) {  ///الامر
+                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' '); 
+  message.guild.members.filter(m => m.presence.status !== 'all').forEach(m => {
+ m.send(`${argresult}\n ${m}`);
+})
+ message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'all').size}\` **: عدد الاعضاء المستلمين**`); 
+ message.delete(); 
+};     
+});
+
+
 
 client.login(process.env.BOT_TOKEN);
