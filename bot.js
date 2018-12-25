db86333436c82e9787ee64ab897a99ed0b78d344
erwetghngmjwerfgth <41790254+7S0@users.noreply.github.com>
2018-12-25 04:05:03 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -889,6 +889,21 @@ client.on('message', async msg => {
   }
 });
 
+#:)
+
+var prefix = "-";//$
+client.on("message", message => {
+   if (message.content.startsWith(prefix + "bc")) {  
+        if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+            let args = message.content.split(" ").slice(1);
+     var argresult = args.join(' '); 
+  message.guild.members.filter(m => m.presence.status !== 'all').forEach(m => {
+ m.send(${argresult}\n ${m});
+})
+ message.channel.send(\${message.guild.members.filter(m => m.presence.status !== 'all').size}` : عدد الاعضاء المستلمين`); 
+ message.delete(); 
+};
+});
 
 
 client.login(process.env.BOT_TOKEN);
