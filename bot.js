1d54de2736651eafd0c1599957318639549d1d44
erwetghngmjwerfgth <41790254+7S0@users.noreply.github.com>
2018-12-25 17:08:45 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -503,7 +503,7 @@ client.on('message', message => {
 
     let args = message.content.split(' ').slice(1).join(' ');
      if(!message.channel.guild) return;
-if(message.content.split(' ')[0] == '*bc') {
+if(message.content.split(' ')[0] == '$bc') {
 	if (!message.member.hasPermission("ADMINISTRATOR"))  return;
          message.react("✔️")
           let embed = new Discord.RichEmbed()
