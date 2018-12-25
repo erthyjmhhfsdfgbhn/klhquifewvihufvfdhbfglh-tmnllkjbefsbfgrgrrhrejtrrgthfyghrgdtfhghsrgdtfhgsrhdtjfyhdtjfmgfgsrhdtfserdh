dfc499cbb0524d096f78704282c40ef64cfc1c6a
erwetghngmjwerfgth <41790254+7S0@users.noreply.github.com>
2018-12-25 15:11:28 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -353,30 +353,6 @@ client.on('message', message => {
     }
 });
 
-client.on('message', message => {
-   let embed = new Discord.RichEmbed()
-
-    let args = message.content.split(' ').slice(1).join(' ');
-     if(!message.channel.guild) return;
-if(message.content.split(' ')[0] == '$bc') {
-    if (!message.member.hasPermission("ADMINISTRATOR"))  return;
-         message.react("heavy_check_mark️")
-          let embed = new Discord.RichEmbed()
-    .setColor("#FF00FF")
-    .setThumbnail(message.author.avatarURL)   
-                                      .addField('تم الارسال بواسطة :', "<@" + message.author.id + ">")
-                           message.channel.send(***${message.guild.members.filter(m => m.presence.status !== 'all').size}$***: عدد الاعضاء المستلمين); 
-                 message.channel.sendEmbed(embed);
-        message.guild.members.forEach(m => {
-            var bc = new Discord.RichEmbed()
-.addField('● Sender  :', *** → ${message.author.username}#${message.author.discriminator}***)
-            .addField('● Server  :', *** → ${message.guild.name}***)               
-    .setColor('#ff0000')
-                 .addField('ّ', args)
-            m.send(``,{embed: bc});
-        });
-    }
-}) 
 
 
 
