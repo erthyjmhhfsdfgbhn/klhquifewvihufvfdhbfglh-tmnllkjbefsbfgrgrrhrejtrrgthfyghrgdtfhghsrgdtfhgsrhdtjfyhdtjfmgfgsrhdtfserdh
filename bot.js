1b7058009e1f2a5ee9eaa56f788c9112f909a656
erwetghngmjwerfgth <41790254+7S0@users.noreply.github.com>
2018-12-25 18:19:22 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -523,4 +523,21 @@ if(message.content.split(' ')[0] == '$bc') {
 })
 
 
+client.on('message', message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith($)) return;
+
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+  if (command == "say") {
+   message.channel.sendMessage(args.join("  "))
+   message.delete()
+  }
+ });
+
+
+
 client.login(process.env.BOT_TOKEN);
